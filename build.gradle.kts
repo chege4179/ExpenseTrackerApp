f19b35d5dcbb853e1b13ac72bc32a62e47486896
Peter Chege <peterkagure@gmail.com>
2023-02-08 00:48:21 +0300

    Added spotless and workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2023 Expense Tracker App By Peter Chege
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.jetbrains.kotlin.js.translate.context.Namer.kotlin
+
 buildscript {
     dependencies {
         classpath ("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
@@ -7,4 +24,22 @@ plugins {
     id ("com.android.application") version "7.4.1" apply false
     id ("com.android.library") version "7.4.1" apply false
     id ( "org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("com.diffplug.spotless") version "5.3.0"
+}
+
+apply(plugin = "com.diffplug.spotless")
+spotless {
+    kotlin {
+        target("**/*.kt")
+        licenseHeaderFile(
+            rootProject.file("${project.rootDir}/spotless/LICENSE.txt"),
+            "^(package|object|import|interface)"
+        )
+    }
+    format("kts") {
+        target("**/*.kts")
+        targetExclude("**/build/**/*.kts")
+        // Look for the first line that doesn't have a block comment (assumed to be the license)
+        licenseHeaderFile(rootProject.file("spotless/LICENSE.txt"), "(^(?![\\/ ]\\*).*$)")
+    }
 }
