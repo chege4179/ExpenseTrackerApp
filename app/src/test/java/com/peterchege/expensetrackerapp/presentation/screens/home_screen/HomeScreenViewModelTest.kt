15149cddf0766f07039587fd73a5d68ee268dfdc
Peter Chege <peterkagure@gmail.com>
2023-02-26 17:01:14 +0300

    Added more test

---
+++ app/src/test/java/com/peterchege/expensetrackerapp/presentation/screens/home_screen/HomeScreenViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.peterchege.expensetrackerapp.presentation.screens.home_screen
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class HomeScreenViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
---
+++ app/src/test/java/com/peterchege/expensetrackerapp/repository/FakeTransactionRepository.kt
@@ -0,0 +1,4 @@
+package com.peterchege.expensetrackerapp.repository
+
+class FakeTransactionRepository {
+}
