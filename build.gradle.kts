7f861f93da6bcd97d83ef548cdcc8a0495e6fd78
Peter Chege <peterkagure@gmail.com>
2024-08-17 21:06:06 +0300

    Remove compose reports in build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,21 +62,6 @@ subprojects {
         buildUponDefaultConfig = true
     }
 
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        kotlinOptions {
-            if (project.findProperty("enableComposeCompilerReports") == "true") {
-                freeCompilerArgs += listOf(
-                    "-P",
-                    "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" +
-                            project.buildDir.absolutePath + "/compose_metrics"
-                )
-                freeCompilerArgs += listOf(
-                    "-P",
-                    "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" +
-                            project.buildDir.absolutePath + "/compose_metrics"
-                )
-            }
-        }
-    }
+
 
 }
